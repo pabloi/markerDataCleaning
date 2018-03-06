b9989517dff1139c5c595ff4c007d01f1eeb6621
Unknown <itu.pablo@gmail.com>
2018-03-06 12:44:05 -0500

    Deleted unused code

--- fun/getPositionFromDistances_v2.m
+++ fun/getPositionFromDistances_v2.m
@@ -32,30 +32,12 @@ pos=fminunc(@(x) distanceDistanceAllNew(reshape(x,M,dim),knownPositions,knownDis
 
 end
 
-% function [f,g]=distanceDistance(x,kP,kD,w)
-%     xx=bsxfun(@minus,x,kP); %Relative positions
-%     normXX=sqrt(sum(xx.^2,2));
-%     f=norm(w.*(normXX-kD))^2;
-%     gg1=2*w.^2.*(normXX-kD);
-%     gg2=bsxfun(@rdivide,xx,normXX);
-%     gg=bsxfun(@times,gg1,gg2);
-%     g=sum(gg,1);
-% end
-
 %Old: (cuadratic weighing of distances)
 %Only considers distances from the unknown markers to the known ones, but
 %not between the unknown ones (where arguably we can also have priors)
 function [f,g]=distanceDistanceAll(x,kP,kD,w)
     [M,dim]=size(x);
     [N,dim]=size(kP);
-%     xx=bsxfun(@minus,x,reshape(kP',1,size(kP,2),size(kP,1))); %M x dim x N
-%     normXX=sqrt(sum(xx.^2,2)); %M x 1 x N
-%     nD=reshape(normXX,M,N);
-%     f=sum(sum((w'.*(nD-kD')).^2)); %scalar
-%     gg1=2*w'.^2.*(nD-kD'); %M x N
-%     gg2=bsxfun(@rdivide,xx,normXX); %M x dim x N
-%     gg=bsxfun(@times,reshape(gg1,size(gg2,1),1,size(gg2,3)),gg2); %M x dim x N
-%     g=sum(gg,3); %M x dim
     [nD,gx]=pos2Dist(x,kP);
     f=sum(sum((w'.*(nD-kD')).^2)); %scalar
     g=reshape(2*sum(w'.^2.*(nD-kD').*gx,2),M,dim); %Gradient with respect to x
@@ -87,30 +69,8 @@ function [D,gx]=pos2Dist(x,y)
     if nargout>1 %Computing gradients too
                 D1=D;
         D1(D==0)=1;
-        gx=bsxfun(@rdivide,xx,D1); %NxMxdim
+        gx=bsxfun(@rdivide,xx,D1); %NxMxdim -> This gradient is wrong if nargin==1
         %gx=xx./d; %allowed in R2017a and newer
         %gy=-gx; -> Gradients are opposite to one another if we preserve the shape
     end
 end
-
-%% A little script to test distanceDistanceAll:
-% X1=randn(10,3);
-% D=computeDistanceMatrix(X1);
-% kP=X1(1:7,:);
-% kD=D(1:7,8:10);
-% w=ones(size(kD));
-% 
-% %% Eval:
-% xA=randn(3,3);
-% [fA,gA]=distanceDistanceAll(xA,kP,kD,w);
-% xB=bsxfun(@plus,xA,[0, 0, 1e-5]);
-% [fB,gB]=distanceDistanceAll(xB,kP,kD,w);
-% xC=bsxfun(@plus,xA,[0, 1e-5, 0]);
-% [fC,gC]=distanceDistanceAll(xC,kP,kD,w);
-% xD=bsxfun(@plus,xA,[1e-5, 0, 0]);
-% [fD,gD]=distanceDistanceAll(xD,kP,kD,w);
-% sum(reshape(gA,3,3),1)
-% 
-% [(fD-fA) (fC-fA) (fB-fA)]/1e-5
-% 
-% [fA,~]=distanceDistanceAll(X1(8:10,:),kP,kD,w)
