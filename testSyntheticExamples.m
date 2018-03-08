3b9f4a14bd647e23651d84743593a5e8708dc6f4
Unknown <itu.pablo@gmail.com>
2018-03-07 20:34:52 -0500

    Added skeleton for synthetic testing

---
+++ testSyntheticExamples.m
@@ -0,0 +1,12 @@
+
+%%
+for i=1:N % Delete a marker, reconstruct it (fast) and compare
+
+end
+
+%% Select a pair of labels and switch, see which ones we can detect
+
+
+%% Randomly move markers, test outlier detection
+
+%% Randomly move markers, test reconstruction
